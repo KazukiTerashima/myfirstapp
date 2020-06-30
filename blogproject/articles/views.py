1296a44ee4441e6624505403dd43fe257425b8fa
KazukiTerashima <terashima.kazuki@outlook.jp>
2020-06-30 23:55:55 +0900

    my first try

---
+++ blogproject/articles/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, CreateView
+
+from .models import Article
+from .forms import ArticleForm
+
+
+class ArticleListView(ListView):
+    template_name = 'articles/articles_list.html'
+    model = Article
+
+
+class ArticleCreateView(CreateView):
+    template_name = 'articles/articles_create.html'
+    model = Article
+    form_class = ArticleForm
+    success_url = '/articles/'
